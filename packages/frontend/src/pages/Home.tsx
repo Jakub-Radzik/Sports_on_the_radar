c0dc06cb5b13279aa8ff0eea82026c4d56f5b374
Jakub Radzik <jradzik4@gmail.com>
2023-09-02 14:42:33 +0200

    Display Simulation in home page

---
+++ packages/frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { SimulationCard } from '../features/matchSimulations/components/SimulationCard';
+
+export default function Home() {
+  return (
+    <div>
+      <SimulationCard />
+    </div>
+  );
+}
